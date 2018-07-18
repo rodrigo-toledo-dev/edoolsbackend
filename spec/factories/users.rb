a79e3a396bc88e8b0f7a7df4fcbdeb7e30903d64
Rodrigo Toledo <agc.rodrigo@gmail.com>
2018-07-17 21:58:05 -0300

    send instructions to forgot password, tests and layouts adjusts. Need change default color to blue

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email 'rodrigo.toledo@edools.com.br'
+    email 'rodrigo.toledo@eadbox.com'
     password 'asdqwe123'
     password_confirmation 'asdqwe123'
   end
